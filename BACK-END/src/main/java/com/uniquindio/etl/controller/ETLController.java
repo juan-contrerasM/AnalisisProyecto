2a0486c52a3a4c01d10d0a4e3b6b0145bc241e29
julian <julianandresnaranjoalzate@gmail.com>
2026-05-03 17:44:20 -0500

    edit in ETLController

--- BACK-END/src/main/java/com/uniquindio/etl/controller/ETLController.java
+++ BACK-END/src/main/java/com/uniquindio/etl/controller/ETLController.java
@@ -7,10 +7,7 @@ import com.uniquindio.etl.sorting.SortingBenchmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -19,6 +16,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/etl")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "https://analisis-proyecto-frontend.vercel.app")
 public class ETLController {
 
     private final SortingBenchmarkService sortingBenchmarkService;
